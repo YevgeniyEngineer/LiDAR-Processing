a29ba68a9e45d602d8873825cebc6fc026257000
Yevgeniy <simonov.yevgeniy@gmail.com>
2022-11-18 19:27:06 +0800

    style: code refactoring

--- lidar_processing/data_reader_node/src/data_reader_node.cpp
+++ lidar_processing/data_reader_node/src/data_reader_node.cpp
@@ -1,8 +1,21 @@
 #include "data_reader_node.hpp"
 
-std::string filepath = "/home/yevgeniy/Documents/GitHub/LiDAR-Processing/data";
+namespace lidar_processing
+{
+PointCloudPublisher::PointCloudPublisher() : Node("point_cloud_publisher_node")
+{
+    std::cout << "PointCloudPublisher node started." << std::endl;
 
-std::vector<std::filesystem::path> readFilenamesExt(std::filesystem::path const &root, std::string const &ext)
+    publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("point_cloud_topic", 10);
+    timer_ =
+        this->create_wall_timer(std::chrono::milliseconds(100), std::bind(&PointCloudPublisher::timerCallback, this));
+
+    filenames_ = readFilenamesExt(filepath, ".pcd");
+    filenames_iterator_ = filenames_.begin();
+}
+
+std::vector<std::filesystem::path> PointCloudPublisher::readFilenamesExt(std::filesystem::path const &root,
+                                                                         std::string const &ext)
 {
     std::vector<std::filesystem::path> paths = {};
 
@@ -25,93 +38,71 @@ std::vector<std::filesystem::path> readFilenamesExt(std::filesystem::path const 
     return paths;
 }
 
-// Reads pcd data on repeat at 10Hz
-class PointCloudPublisher : public rclcpp::Node
+void PointCloudPublisher::timerCallback()
 {
-  public:
-    PointCloudPublisher() : Node("point_cloud_publisher_node")
+
+    sensor_msgs::msg::PointCloud2::Ptr message = std::make_shared<sensor_msgs::msg::PointCloud2>();
+
+    // Reset iterator
+    if (filenames_iterator_ == filenames_.end())
     {
-        std::cout << "PointCloudPublisher node started." << std::endl;
-
-        publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("point_cloud_topic", 10);
-        timer_ = this->create_wall_timer(std::chrono::milliseconds(100),
-                                         std::bind(&PointCloudPublisher::timer_callback, this));
-
-        filenames_ = readFilenamesExt(filepath, ".pcd");
         filenames_iterator_ = filenames_.begin();
     }
 
-  private:
-    void timer_callback()
+    // Read pointcloud
+    filename_ = (*filenames_iterator_).string();
+    pcl::PointCloud<pcl::PointXYZI>::Ptr pcl_message = std::make_shared<pcl::PointCloud<pcl::PointXYZI>>();
+    if (pcl::io::loadPCDFile<pcl::PointXYZI>(filename_, *pcl_message) == -1)
     {
-
-        sensor_msgs::msg::PointCloud2::Ptr message = std::make_shared<sensor_msgs::msg::PointCloud2>();
-
-        // Reset iterator
-        if (filenames_iterator_ == filenames_.end())
-        {
-            filenames_iterator_ = filenames_.begin();
-        }
-
-        // Read pointcloud
-        filename_ = (*filenames_iterator_).string();
-        pcl::PointCloud<pcl::PointXYZI>::Ptr pcl_message = std::make_shared<pcl::PointCloud<pcl::PointXYZI>>();
-        if (pcl::io::loadPCDFile<pcl::PointXYZI>(filename_, *pcl_message) == -1)
-        {
-            std::cout << "Clouldn't read .pcd file" << std::endl;
-            return;
-        }
-        // else
-        // {
-        //     std::cout << "Successfully read " << filename_ << std::endl;
-        // }
-
-        // pcl::PointCloud to sensor_msgs::msg::PointCloud2
-        if (pcl_message->width == 0 && pcl_message->height == 0)
-        {
-            message->width = pcl_message->size();
-            message->height = 1;
-        }
-        else
-        {
-            assert(pcl_message->size() == pcl_message->width * pcl_message->height);
-            message->height = pcl_message->height;
-            message->width = pcl_message->width;
-        }
-
-        // Fill point cloud binary data (padding and all)
-        std::size_t data_size = sizeof(pcl::PointXYZI) * pcl_message->size();
-        message->data.resize(data_size);
-        if (data_size)
-        {
-            std::memcpy(&(message->data[0]), &(*pcl_message)[0], data_size);
-        }
-
-        // Fill metadata
-        message->fields.clear();
-        message->point_step = sizeof(pcl::PointXYZI);
-        message->row_step = sizeof(pcl::PointXYZI) * message->width;
-        message->is_dense = pcl_message->is_dense;
-        message->is_bigendian = false;
-
-        // Publish poincloud
-        publisher_->publish(*message);
-
-        // Increment iterator
-        ++filenames_iterator_;
+        std::cout << "Clouldn't read .pcd file" << std::endl;
+        return;
+    }
+    else
+    {
+        std::cout << "Successfully read " << filename_ << std::endl;
     }
 
-    std::shared_ptr<rclcpp::TimerBase> timer_;
-    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher_;
-    std::vector<std::filesystem::path> filenames_;
-    std::vector<std::filesystem::path>::iterator filenames_iterator_;
-    std::string filename_;
-};
+    // pcl::PointCloud to sensor_msgs::msg::PointCloud2
+    if (pcl_message->width == 0 && pcl_message->height == 0)
+    {
+        message->width = pcl_message->size();
+        message->height = 1;
+    }
+    else
+    {
+        assert(pcl_message->size() == pcl_message->width * pcl_message->height);
+        message->height = pcl_message->height;
+        message->width = pcl_message->width;
+    }
+
+    // Fill point cloud binary data (padding and all)
+    std::size_t data_size = sizeof(pcl::PointXYZI) * pcl_message->size();
+    message->data.resize(data_size);
+    if (data_size)
+    {
+        std::memcpy(&(message->data[0]), &(*pcl_message)[0], data_size);
+    }
+
+    // Fill metadata
+    message->fields.clear();
+    message->point_step = sizeof(pcl::PointXYZI);
+    message->row_step = sizeof(pcl::PointXYZI) * message->width;
+    message->is_dense = pcl_message->is_dense;
+    message->is_bigendian = false;
+
+    // Publish poincloud
+    publisher_->publish(*message);
+
+    // Increment iterator
+    ++filenames_iterator_;
+}
+
+} // namespace lidar_processing
 
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<PointCloudPublisher>());
+    rclcpp::spin(std::make_shared<lidar_processing::PointCloudPublisher>());
     rclcpp::shutdown();
     return 0;
 }
---
+++ lidar_processing/data_reader_node/src/data_reader_node.hpp
@@ -0,0 +1,65 @@
+#pragma once
+#ifndef DATA_READER_NODE_HPP_
+#define DATA_READER_NODE_HPP_
+
+// std
+#include <chrono>
+#include <filesystem>
+#include <functional>
+#include <memory>
+#include <string>
+#include <vector>
+
+// ROS2
+#include "rclcpp/publisher.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp/timer.hpp"
+#include <sensor_msgs/msg/point_cloud.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <sensor_msgs/point_cloud_conversion.hpp>
+
+// PCL
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/conversions.h>
+#include <pcl/for_each_type.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+// Data path
+std::string filepath = "/home/yevgeniy/Documents/GitHub/LiDAR-Processing/data";
+
+namespace lidar_processing
+{
+// Reads pcd data on repeat at 10Hz
+class PointCloudPublisher : public rclcpp::Node
+{
+  public:
+    // constructor
+    PointCloudPublisher();
+
+    // destructor
+    ~PointCloudPublisher() = default;
+
+  private:
+    // timer callback function used by the timer
+    void timerCallback();
+
+    // function that reads file names
+    std::vector<std::filesystem::path> readFilenamesExt(std::filesystem::path const & /* path */,
+                                                        std::string const & /* extension */);
+
+    // message published by this node
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr publisher_;
+
+    // timer
+    std::shared_ptr<rclcpp::TimerBase> timer_;
+
+    // for file reading and iteration
+    std::string filename_;
+    std::vector<std::filesystem::path> filenames_;
+    std::vector<std::filesystem::path>::iterator filenames_iterator_;
+};
+} // namespace lidar_processing
+
+#endif // DATA_READER_NODE_HPP_
