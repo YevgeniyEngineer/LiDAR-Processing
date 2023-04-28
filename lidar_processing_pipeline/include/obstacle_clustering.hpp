c87801b781f3989995145e92db1bbfa3990a5c13
Yevgeniy <yevgeniy.engineer@gmail.com>
2023-04-28 22:21:58 +0800

    Replaced fast euclidean clustering with a custom version

--- lidar_processing_pipeline/include/obstacle_clustering.hpp
+++ lidar_processing_pipeline/include/obstacle_clustering.hpp
@@ -3,7 +3,7 @@
 
 // Required libraries
 #include "dbscan.hpp"
-#include "fast_euclidean_clustering.hpp"
+#include "fec_clustering.hpp"
 
 // std
 #include <algorithm>
@@ -156,47 +156,58 @@ void ObstacleClusterer::clusterObstacles(const pcl::PointCloud<PointT> &cloud,
     }
     else if (clustering_algorithm_ == ClusteringAlgorithm::FAST_EUCLIDEAN_CLUSTERING)
     {
-        // Prepare points for clustering
-        pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
-        input_cloud->points.reserve(cloud.size());
+        constexpr static auto number_of_dimensions = 3UL;
+        using CoordinateType = float;
+        using FECPoint = clustering::FECPoint<CoordinateType, number_of_dimensions>;
+        using FECPointCloud = clustering::FECPointCloud<CoordinateType, number_of_dimensions>;
+        using FECClustering = clustering::FECClustering<CoordinateType, number_of_dimensions>;
+
+        // Convert to required format
+        auto input_cloud = std::make_unique<FECPointCloud>();
+        input_cloud->reserve(cloud.size());
         for (const auto &point : cloud.points)
         {
-            pcl::PointXYZ point_cache{point.x, point.y, point.z};
-            input_cloud->points.emplace_back(std::move(point_cache));
+            input_cloud->push_back(FECPoint{point.x, point.y, point.z});
         }
-        input_cloud->height = 1;
-        input_cloud->width = input_cloud->points.size();
 
-        // Perform clustering and store cluster indices
-        pcl::search::KdTree<pcl::PointXYZ>::Ptr kdtree = std::make_shared<pcl::search::KdTree<pcl::PointXYZ>>();
+        // Prepare clustering
+        auto fec_clustering = std::make_unique<FECClustering>(*input_cloud);
+        fec_clustering->quality(cluster_quality_);
+        fec_clustering->minClusterSize(min_cluster_size_);
+        fec_clustering->clusterTolerance(neighbour_radius_threshold_);
 
-        std::vector<pcl::PointIndices> cluster_indices;
-        FastEuclideanClustering<pcl::PointXYZ> fast_euclidean_clustering;
-        fast_euclidean_clustering.setInputCloud(input_cloud);
-        fast_euclidean_clustering.setSearchMethod(kdtree);
-        fast_euclidean_clustering.setClusterTolerance(neighbour_radius_threshold_);
-        fast_euclidean_clustering.setQuality(cluster_quality_);
-        fast_euclidean_clustering.setMinClusterSize(min_cluster_size_);
-        fast_euclidean_clustering.segment(cluster_indices);
+        // Form clusters and get results
+        fec_clustering->formClusters();
+        const auto clusters = fec_clustering->getClusterIndices();
 
-        // Copy clustered points
+#if DEBUG_CLUSTERING
+        std::cout << "Created " << clusters.size() << " clusters." << std::endl;
+#endif
+
+        // Copy points to output cloud
         clustered_cloud.clear();
-        clustered_cloud.reserve(cluster_indices.size());
-        for (const auto &cluster_index_list : cluster_indices)
+        clustered_cloud.reserve(clusters.size());
+        for (const auto &cluster_indices : clusters)
         {
+            // Add non-noise points
             auto cluster_points = std::make_unique<pcl::PointCloud<pcl::PointXYZ>>();
-            cluster_points->reserve(cluster_index_list.indices.size());
-            for (const auto &index : cluster_index_list.indices)
+            cluster_points->reserve(cluster_indices.second.size());
+            for (const auto &cluster_index : cluster_indices.second)
             {
-                const pcl::PointXYZ &point_xyz = input_cloud->points[index];
-                pcl::PointXYZ point_cache{point_xyz.x, point_xyz.y, point_xyz.z};
-                cluster_points->emplace_back(std::move(point_cache));
+                // Get the point
+                const auto &point = (*input_cloud)[cluster_index];
+                pcl::PointXYZ point_cache{point[0], point[1], point[2]};
+                cluster_points->push_back(std::move(point_cache));
             }
             cluster_points->height = 1;
             cluster_points->width = cluster_points->points.size();
-            clustered_cloud.emplace_back(std::move(*cluster_points));
+            clustered_cloud.push_back(std::move(*cluster_points));
             cluster_points.reset();
         }
+
+#if DEBUG_CLUSTERING
+        std::cout << "Copied " << clustered_cloud.size() << " clusters." << std::endl;
+#endif
     }
 }
 
